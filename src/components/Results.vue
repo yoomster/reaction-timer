3fe03bbb9db97ee1209d79c9cd94e71852182963
yoomster <nperenboom@hotmail.com>
2025-05-05 00:11:27 +0200

    final game

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,5 +1,6 @@
 <template>
   <p>Reaction time: {{ speed }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
@@ -7,8 +8,24 @@ export default {
   props: ["speed"],
 
   data() {
-    return {};
+    return {
+      rank: null,
+    };
   },
+  mounted() {
+    if (this.speed < 250) this.rank = "Ninja finger";
+    else if (this.speed < 400) this.rank = "that's an ok finger";
+    else this.rank = "snail finger...";
+  },
+
   methods: {},
 };
 </script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
